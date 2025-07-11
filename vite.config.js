8394d3cd396e7ef4744360598603132a1e81b6a6
fake-satan101 <lalligah@gmail.com>
2025-07-12 03:44:55 +0530

    edited package and config fies

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Search2/', // <-- replace with your repo name
+  base: process.env.VITE_BASE_PATH ||'/Search2', // <-- replace with your repo name
 });
