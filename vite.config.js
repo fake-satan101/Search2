d62c159a79026fe42e495ababd69afa7b52f39a7
fake-satan101 <lalligah@gmail.com>
2025-07-12 01:37:43 +0530

    Use minimal deploy-pages@v1 workflow

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Search2/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify'
+    }
+  }
 })
