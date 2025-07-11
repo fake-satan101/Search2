2c0fa5386d6649233bd4e25cc895e7df33efe170
fake-satan101 <lalligah@gmail.com>
2025-07-12 03:33:46 +0530

    edited package and config fies

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Search2/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      crypto: 'crypto-browserify'
-    }
-  }
-})
+  base: '/Search2/', // <-- replace with your repo name
+});
